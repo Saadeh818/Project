6858f8c4601f196b5206b8a62cec3e4d35a16051
ahmadmsaadeh <ahmadmalek169@yahoo.com>
2023-10-11 15:44:42 +0300

    Adding Home page Feature

---
+++ src/test/java/org/example/AcceptanceTest/AdminProductManagement.java
@@ -0,0 +1,34 @@
+package org.example.AcceptanceTest;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class AdminProductManagement {
+    @When("admin in product management entered {string}")
+    public void adminInProductManagementEntered(String arg0) {
+    }
+    @Then("add product")
+    public void add_product() {
+
+    }
+
+    @Then("delete product")
+    public void delete_product() {
+
+    }
+
+    @Then("update product")
+    public void update_product() {
+
+    }
+
+    @Then("go to dashboard")
+    public void go_to_dashboard() {
+
+    }
+
+    @Then("reload product manager")
+    public void reload_product_manager() {
+
+    }
+}
