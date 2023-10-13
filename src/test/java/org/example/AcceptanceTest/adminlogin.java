177ce4acd03b513f2b4662cbcb621c33db2adb23
deema-saleh <132137400+deema-saleh@users.noreply.github.com>
2023-10-13 21:29:47 +0300

    Add files via upload

---
+++ src/test/java/org/example/AcceptanceTest/adminlogin.java
@@ -0,0 +1,37 @@
+package org.example.firstproject.AcceptanceTest;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class adminlogin {
+
+    @Given("that the admin is not logged in the app")
+    public void thatTheAdminIsNotLoggedInTheApp() {
+
+    }
+
+    @Then("the admin is logged in the app successfully")
+    public void theAdminIsLoggedInTheAppSuccessfully() {
+
+    }
+    @Given("the username is {string}")
+    public void theUsernameIs(String string) {
+
+    }
+    @Given("the password is {string}")
+    public void thePasswordIs(String string) {
+
+    }
+    @Then("the admin will not login")
+    public void theAdminWillNotLogin() {
+
+    }
+    @Then("the message appear to tell the admin what's wrong")
+    public void theMessageAppearToTellTheAdminWhatSWrong() {
+
+    }
+}
