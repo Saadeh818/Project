da2e4c4a51e587114c983de123f31fc0cba629d2
deema-saleh <132137400+deema-saleh@users.noreply.github.com>
2023-10-14 18:49:48 +0300

    Add files via upload

---
+++ src/test/java/org/example/AcceptanceTest/customerCRUD.java
@@ -0,0 +1,55 @@
+package org.example.firstproject.AcceptanceTest;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class customerCRUD {
+
+    @Given("customer tab is opened")
+    public void customerTabIsOpened() {
+
+    }
+    @Given("I select a customer from the list")
+    public void iSelectACustomerFromTheList() {
+
+    }
+    @Given("I click delete customer button")
+    public void iClickDeleteCustomerButton() {
+
+    }
+    @Then("the selected customer is deleted")
+    public void theSelectedCustomerIsDeleted() {
+    }
+    @Given("I enter a different customer information")
+    public void iEnterADifferentCustomerInformation() {
+
+    }
+    @Given("I click update customer button")
+    public void iClickUpdateCustomerButton() {
+
+    }
+    @Then("the selected customer is updated")
+    public void theSelectedCustomerIsUpdated() {
+
+    }
+    @Given("I enter customer information")
+    public void iEnterCustomerInformation() {
+
+    }
+    @Given("the name is not empty")
+    public void theNameIsNotEmpty() {
+
+    }
+    @Given("I click add customer button")
+    public void iClickAddCustomerButton() {
+
+    }
+    @Then("a new customer is added to the list")
+    public void aNewCustomerIsAddedToTheList() {
+
+    }
+
+
+
+
+
+}
