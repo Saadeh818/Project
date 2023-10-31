79da80e324944f4a1de7baed97378102ed550ac4
ahmadmsaadeh <ahmadmalek169@yahoo.com>
2023-10-31 15:13:36 +0200

    adding more features to admin

--- src/main/java/org/example/AcceptanceTest/ProductC.java
+++ src/main/java/org/example/AcceptanceTest/ProductC.java
@@ -1,6 +1,11 @@
 package org.example.AcceptanceTest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductC {
+    private static int nextProductId = 1;
+    public int productId;
     public static boolean readyToDelete;
     public static boolean readyToUpdate;
     public static boolean wrongInputFlag;
@@ -10,6 +15,28 @@ public class ProductC {
     public int price;
     public boolean productAddSuccessfully;
     public static boolean readyToAdd=false;
+    public List<ProductC> productList;
+
+    public ProductC() {
+        productList = new ArrayList<>();
+        productList.add(new ProductC("cat1", "product1", 18, 100));
+        productList.add(new ProductC("cat1", "product2", 85, 150));
+        productList.add(new ProductC("cat2", "product3", 44, 480));
+        productList.add(new ProductC("cat3", "product4", 48, 940));
+        productList.add(new ProductC("cat1", "product5", 10, 613));
+        productList.add(new ProductC("cat3", "product6", 88, 984));
+        productList.add(new ProductC("cat2", "product7", 91, 513));
+        productList.add(new ProductC("cat1", "product8", 77, 560));
+        productList.add(new ProductC("cat2", "product9", 24, 613));
+    }
+
+    public ProductC(String category, String name, int quantity, int price) {
+        this.productId = nextProductId++;
+        this.category = category;
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
 
     public static void productManager(String userInput) {
         switch (userInput){
@@ -51,6 +78,14 @@ public class ProductC {
     }
 
     public void productList() {
+        for (ProductC product : productList) {
+            System.out.println("Product ID: " + product.productId);
+            System.out.println("Product Category: " + product.category);
+            System.out.println("Product Name: " + product.name);
+            System.out.println("Product Quantity: " + product.quantity);
+            System.out.println("Product Price: " + product.price);
+            System.out.println(); // Separate each product with a newline
+        }
     }
 
     public void addProduct(String categoryI, String nameI, String quantityI, String priceI) {
@@ -62,10 +97,11 @@ public class ProductC {
                 name=nameI;
                 quantity = Integer.parseInt(quantityI);
                 price=Integer.parseInt(priceI);
+                ProductC product = new ProductC(category, name, quantity, price);
+                productList.add(product);
                 productAddSuccessfully=true;
+
             }
-
-
         }
         catch (Exception ignored){
             productAddSuccessfully=false;
