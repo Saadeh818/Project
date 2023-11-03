2fc974602bbf3d184decf9642c1d2e3ed216262b
deema-saleh <132137400+deema-saleh@users.noreply.github.com>
2023-11-03 03:41:48 +0200

    commite

--- src/test/java/org/example/AcceptanceTest/Review.java
+++ src/test/java/org/example/AcceptanceTest/Review.java
@@ -1,104 +1,98 @@
 package org.example.AcceptanceTest;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 
+
 public class Review {
+   
     @Given("that the user has purchased a product")
     public void thatTheUserHasPurchasedAProduct() {
-
+        // Assuming user has purchased a product
     }
+
     @Given("the user is logged in")
     public void theUserIsLoggedIn() {
-
-    }
-    @Given("that the user has already written a review for a product")
-    public void thatTheUserHasAlreadyWrittenAReviewForAProduct() {
-
-    }
-    @When("the user clicks on the delete review button")
-    public void theUserClicksOnTheDeleteReviewButton() {
-
-    }
-    @Then("the review is removed from the product page")
-    public void theReviewIsRemovedFromTheProductPage() {
-
-    }
-    @Then("a success message is displayed.")
-    public void aSuccessMessageIsDisplayed() {
-
-    }
-    @Given("that the user is on the review form page")
-    public void thatTheUserIsOnTheReviewFormPage() {
-
-    }
-    @When("the user fills out the review form with more than the maximum allowed characters")
-    public void theUserFillsOutTheReviewFormWithMoreThanTheMaximumAllowedCharacters() {
-
-    }
-    @Then("an error message is displayed indicating the maximum review length has been exceeded")
-    public void anErrorMessageIsDisplayedIndicatingTheMaximumReviewLengthHasBeenExceeded() {
-
+        
     }
 
-
-
-
-
-    @When("the user clicks on the edit review button")
-    public void theUserClicksOnTheEditReviewButton() {
-
-    }
-    @When("the user edits the review form")
-    public void theUserEditsTheReviewForm() {
-
-    }
-    @Then("the review is updated on the product page")
-    public void theReviewIsUpdatedOnTheProductPage() {
-
+    @Given("the user is not logged in")
+    public void theUserIsNotLoggedIn() {
+       
     }
 
-    @When("the user submits the form with missing required fields")
-    public void theUserSubmitsTheFormWithMissingRequiredFields() {
-
+    @Given("the user is on the product page")
+    public void theUserIsOnTheProductPage() {
+        // Assuming user is on the product page
     }
-    @Then("an error message is displayed asking the user to fill out all required fields")
-    public void anErrorMessageIsDisplayedAskingTheUserToFillOutAllRequiredFields() {
 
-    }
-    @Given("that the user is not logged in")
-    public void thatTheUserIsNotLoggedIn() {
-
-    }
     @When("the user clicks on the write a review button")
     public void theUserClicksOnTheWriteAReviewButton() {
-
-    }
-    @Then("the user is redirected to the login page")
-    public void theUserIsRedirectedToTheLoginPage() {
-
-    }
-    @Then("a message prompts the user to log in first")
-    public void aMessagePromptsTheUserToLogInFirst() {
-
-    }
-
-    @Given("that the user is on the product page")
-    public void thatTheUserIsOnTheProductPage() {
-
+        // Assuming user clicks on the write a review button
     }
 
     @When("the user fills out the review form")
     public void theUserFillsOutTheReviewForm() {
-
+        // Assuming user fills out the review form
     }
+
+    @When("submits the form")
+    public void submitsTheForm() {
+        // Assuming user submits the form
+    }
+
     @Then("the review is added to the product page")
     public void theReviewIsAddedToTheProductPage() {
-
+        // Assuming the review is added to the product page
     }
 
+    @Then("a success message is displayed")
+    public void aSuccessMessageIsDisplayed() {
+        // Assuming a success message is displayed
+    }
+
+    @When("the user clicks on the write a review button without logging in")
+    public void theUserClicksOnTheWriteAReviewButtonWithoutLoggingIn() {
+        // Assuming user clicks on the write a review button without logging in
+    }
+
+    @Then("the user is redirected to the login page")
+    public void theUserIsRedirectedToTheLoginPage() {
+        // Assuming the user is redirected to the login page
+    }
+
+    @Then("a message prompts the user to log in first")
+    public void aMessagePromptsTheUserToLogInFirst() {
+        // Assuming a message prompts the user to log in first
+    }
+
+    @When("the user submits the form with missing required fields")
+    public void theUserSubmitsTheFormWithMissingRequiredFields() {
+        // Assuming the user submits the form with missing required fields
+    }
+
+    @Then("an error message is displayed asking the user to fill out all required fields")
+    public void anErrorMessageIsDisplayedAskingTheUserToFillOutAllRequiredFields() {
+        // Assuming an error message is displayed asking the user to fill out all required fields
+    }
+
+    @When("the user fills out the review form with more than the maximum allowed characters")
+    public void theUserFillsOutTheReviewFormWithMoreThanTheMaximumAllowedCharacters() {
+        // Assuming the user fills out the review form with more than the maximum allowed characters
+    }
+
+    @Then("an error message is displayed indicating the maximum review length has been exceeded")
+    public void anErrorMessageIsDisplayedIndicatingTheMaximumReviewLengthHasBeenExceeded() {
+        // Assuming an error message is displayed indicating the maximum review length has been exceeded
+    }
+
+    @Given("the user has already written a review for a product")
+    public void theUserHasAlreadyWrittenAReviewForAProduct() {
+        // Assuming the user has already written a review for a product
+    }
 
 
 
