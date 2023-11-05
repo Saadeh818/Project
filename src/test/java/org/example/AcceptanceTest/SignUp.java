07a50d544d309d90407e3446f298d245d8fb9eca
deema-saleh <132137400+deema-saleh@users.noreply.github.com>
2023-11-05 15:44:05 +0200

    commite

--- src/test/java/org/example/AcceptanceTest/SignUp.java
+++ src/test/java/org/example/AcceptanceTest/SignUp.java
@@ -1,39 +1,21 @@
-package org.example.AcceptanceTest;
+package org.example.firstproject.AcceptanceTest;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-import static org.junit.Assert.assertTrue;
-
-public class SignUp {
-
-        @Given("the user is on the sign-up page")
-        public void theUserIsOnTheSignUpPage() {
-
-        }
-        @When("the user enters valid registration details")
-        public void theUserEntersValidRegistrationDetails() {
-
-        }
-        //        ***************************************************************************
-        //        ***************************************************************************
-        //        ***************************************************************************
-        //        ***************************************************************************
-        //duplicated name in other file
-//        @When("submits the form")
-//        public void submitsTheForm() {
-//        }
-        //        ***************************************************************************
-        //        ***************************************************************************
-        //        ***************************************************************************
-        //        ***************************************************************************
-
-
-        @Then("the user sign up successfully")
-        public void theUserSignUpSuccessfully() {
-
-          ;
-        }
+public class signUp {
+    @Given("the user is on the sign-up page")
+    public void theUserIsOnTheSignUpPage() {
 
     }
+    @When("the user enters valid registration details")
+    public void theUserEntersValidRegistrationDetails() {
+
+    }
+    @Then("the user sign up successfully")
+    public void theUserSignUpSuccessfully() {
+
+    }
+}
+
