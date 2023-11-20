78abd13f0b6ecb7eb6b082313b7762446f553232
deema-saleh <132137400+deema-saleh@users.noreply.github.com>
2023-11-20 17:54:50 +0200

    commit

--- src/test/java/org/example/AcceptanceTest/SignUp.java
+++ src/test/java/org/example/AcceptanceTest/SignUp.java
@@ -28,17 +28,13 @@ public class SignUp {
             System.out.println("User Type: " + userType);
         }
 
-        @Then("^you should show please fill all informations$")
+        @Then("^you should show please fill all informations")
         public void showPleaseFillAllInformations() {
             // Implementation to verify that the correct message is displayed
             System.out.println("Verify 'Please fill all information' message is displayed");
         }
 
-        @Then("^You should show please enter another id$")
-        public void showPleaseEnterAnotherId() {
-            // Implementation to verify that the correct message is displayed
-            System.out.println("Verify 'Please enter another ID' message is displayed");
-        }
+
 
         @Then("^You should Show please enter more than five character$")
         public void showPleaseEnterMoreThanFiveCharacter() {
@@ -48,7 +44,7 @@ public class SignUp {
     @Then("You should Show please enter another id")
     public void youShouldShowPleaseEnterAnotherId() {
         // Write code here that turns the phrase above into concrete actions
-        System.out.println("Verify 'Please enter more than five characters' message is displayed");
+        System.out.println("Verify 'Please enter another ID' message is displayed");
     }
         @Then("^The Customer Added Successfully")
         public void showCustomerAddedSuccessfully() {
@@ -58,7 +54,7 @@ public class SignUp {
     @When("you write the {string}, {string}, {string}, {string}, {string}, {string}, {string} and {string} and {string}")
     public void youWriteTheAndAnd(String string, String string2, String string3, String string4, String string5, String string6, String string7, String string8, String string9) {
         // Write code here that turns the phrase above into concrete actions
-      
+
     }
 }
 
