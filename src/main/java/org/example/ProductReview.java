3eb78833a1916c847a658c13d987ad03e7d26440
deema <deemasaleh2952002@gmail.com>
2023-12-21 21:12:38 +0200

    DIMA

---
+++ src/main/java/org/example/ProductReview.java
@@ -0,0 +1,100 @@
+package org.example;
+
+public class ProductReview {
+
+    private boolean userLoggedIn;
+    private String reviewFormText;
+    private int reviewFormRating;
+
+    public void setUserLoggedIn(boolean userLoggedIn) {
+        this.userLoggedIn = userLoggedIn;
+    }
+
+    public void setReviewFormText(String reviewFormText) {
+        this.reviewFormText = reviewFormText;
+    }
+
+    public void setReviewFormRating(int reviewFormRating) {
+        this.reviewFormRating = reviewFormRating;
+    }
+
+    public void logInUser() {
+        // Logic for logging in the user
+        System.out.println("User is logged in");
+        userLoggedIn = true;
+    }
+
+    public void logOutUser() {
+        // Logic for logging out the user
+        System.out.println("User is logged out");
+        userLoggedIn = false;
+    }
+
+    public void navigateToReviewFormPage() {
+        // Logic for navigating to the review form page
+        System.out.println("User is on the review form page");
+    }
+
+    public void clickWriteReviewButton() {
+        // Logic for clicking the write a review button
+        System.out.println("User clicks on the write a review button");
+    }
+
+    public void fillOutReviewForm() {
+        // Logic for filling out the review form
+        System.out.println("User fills out the review form with text: " + reviewFormText + ", rating: " + reviewFormRating);
+    }
+
+    public void submitReviewForm() {
+        // Logic for submitting the review form
+        if (userLoggedIn) {
+            System.out.println("User submits the review form");
+            // Additional logic for submitting the form
+        } else {
+            System.out.println("User is not logged in. Cannot submit the review form.");
+        }
+    }
+
+    public boolean isReviewAdded() {
+        return false;
+    }
+
+    public void navigateToProductPage() {
+    }
+
+    public void clickDeleteReviewButton() {
+    }
+
+    public void verifyReviewRemoved() {
+    }
+
+    public void verifySuccessMessage() {
+    }
+
+    public void verifyReviewUpdated() {
+    }
+
+    public void editReviewForm() {
+    }
+
+    public void clickEditReviewButton() {
+    }
+
+    public void verifyErrorMessageForExceededLength() {
+    }
+
+    public void verifyErrorMessageForMissingFields() {
+    }
+
+    public void submitFormWithMissingFields() {
+    }
+
+    public void displayLogInPrompt() {
+    }
+
+    public void redirectToLoginPage() {
+    }
+
+    // Add other methods for handling different scenarios
+
+}
